2fd49975ad18b742301aa459fb3ceb05b050486e
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-09-18 23:34:46 -0400

    chore(lint & format): remove unnecessary rules and update paths

--- .eslintrc.frontend.js
+++ .eslintrc.frontend.js
@@ -1,23 +1,15 @@
 module.exports = {
+	files: ['./admin/**/*'],
 	$schema: 'https://json.schemastore.org/eslintrc',
-	parser: '@babel/eslint-parser',
-	env: {
-		browser: true,
-		es6: true,
-	},
-	plugins: ['react'],
 	extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
 	parserOptions: {
-		requireConfigFile: false,
-		ecmaVersion: 2018,
+		ecmaVersion: 'latest',
+		sourceType: 'module',
 		ecmaFeatures: {
 			jsx: true,
 		},
-		sourceType: 'module',
-		babelOptions: {
-			presets: ['@babel/preset-react'],
-		},
 	},
+	env: { browser: true, es6: true },
 	settings: {
 		react: {
 			version: 'detect',
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,5 @@
-const frontendESLint = require('./.eslintrc.frontend.js');
-const backendESLint = require('./.eslintrc.backend.js');
-
 module.exports = {
 	$schema: 'https://json.schemastore.org/eslintrc',
-	parserOptions: {
-		ecmaVersion: 2018,
-	},
-	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
-	globals: {
-		strapi: 'readonly',
-	},
-	overrides: [
-		{
-			files: ['server/**/*'],
-			...backendESLint,
-		},
-		{
-			files: ['admin/**/*'],
-			...frontendESLint,
-		},
-	],
+	root: true,
+	overrides: [require('./.eslintrc.backend.js'), require('./.eslintrc.frontend.js')],
 };
