0a446b5a18a6d156bbb799c1ce0a521853af2ef8
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-09-18 23:44:06 -0400

    feat(config): add multi build support

--- server/config/index.js
+++ server/config/index.js
@@ -3,5 +3,10 @@
 const { pluginConfigSchema } = require('./schema.js');
 
 module.exports = {
+	default: () => ({
+		shared: {},
+		builds: [],
+		hooks: {},
+	}),
 	validator: async (config) => await pluginConfigSchema.validate(config),
 };
